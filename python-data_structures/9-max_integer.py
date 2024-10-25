bac8e4aa604863807db3d76e41a8abcb738981e4
Donald Duru <d.duru@alustudent.com>
2024-10-25 08:21:16 -0700

    corrected the code in my 10th python script

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,6 @@ def max_integer(my_list=[]):
     """Find the biggest integer in a list."""
     if not my_list:
         return None
-    
     biggest = my_list[0]  # Start with the first element as the biggest
     for num in my_list:
         if num > biggest:
