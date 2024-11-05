5b115d0684593e50063305eb1a02b51e3bc331f0
Donald Duru <d.duru@alustudent.com>
2024-11-04 20:15:14 -0800

    my 2nd script

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+class Square:
+    """A class that defines a square by its size."""
+
+    def __init__(self, size):
+        """Initialize a new square with the given size.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size  # Private instance attribute
