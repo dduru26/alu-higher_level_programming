b08576f0a296fa59dad31b78662ab10215c7931b
Donald Duru <d.duru@alustudent.com>
2024-11-07 23:40:30 -0800

    6th python function

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class that serves as a base for other geometric classes.
+    """
+    pass
