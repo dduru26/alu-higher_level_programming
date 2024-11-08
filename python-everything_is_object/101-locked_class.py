ce2b4ae37d4449bc8f60993eb40cecfef684ffbd
Donald Duru <d.duru@alustudent.com>
2024-11-07 22:41:50 -0800

    corrected the code in my 3rd python file

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-"""This module defines a LockedClass that only allows a single attribute 'first_name'."""
+"""This module defines a LockedClass that restricts dynamic attribute creation."""
 
 class LockedClass:
+    """
+    A class that only allows the creation of a single dynamic attribute 'first_name'.
+
+    The LockedClass restricts the user from creating any additional attributes
+    dynamically, other than the specified 'first_name' attribute.
+    """
+    
     __slots__ = ['first_name']
