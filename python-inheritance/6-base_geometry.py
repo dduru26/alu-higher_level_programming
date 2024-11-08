9ccb9d73d4e8c92ae8fa96dd493df4452496b2da
Donald Duru <d.duru@alustudent.com>
2024-11-07 23:49:31 -0800

    recreated the 7th python function

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a class called BaseGeometry with an area method
+that raises an exception.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometric shapes. Contains a method `area` that raises
+    an exception, which should be implemented in subclasses.
+    """
+
+    def area(self):
+        """
+        Raises an exception indicating that the area() method is not implemented.
+        """
+        raise Exception("area() is not implemented")
