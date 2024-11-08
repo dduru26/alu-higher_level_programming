e71e1380cedc0417cd47bc973b998802adb8a9c5
Donald Duru <d.duru@alustudent.com>
2024-11-07 23:39:26 -0800

    shortened the character limit on the 5th python function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,8 +7,8 @@ a class that inherited (directly or indirectly) from a specified class.
 
 def inherits_from(obj, a_class):
     """
-    Checks if obj is an instance of a class that inherited (directly or indirectly)
-    from a_class, but not an instance of a_class itself.
+    Checks if obj is an instance of a class that inherited (directly or
+    indirectly) from a_class, but not an instance of a_class itself.
 
     Args:
         obj (object): The object to check.
